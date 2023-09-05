bd33f691aabdf34628dc4c2e350e14f6dd36bc87
abecombe <55338725+abecombe@users.noreply.github.com>
2023-09-05 15:08:14 +0900

    Update DispatchHelper.hlsl

--- DispatchHelper.hlsl
+++ DispatchHelper.hlsl
@@ -1,8 +1,8 @@
 #ifndef CS_DISPATCH_HELPER_HLSL
 #define CS_DISPATCH_HELPER_HLSL
 
-int3 _NumThreads;
-int3 _NumGroups;
+uint3 _NumThreads;
+uint3 _NumGroups;
 
 #define RETURN_IF_INVALID(TID) if (any(TID >= _NumThreads)) return;
 
